350524da3203607241d5d9a4a8da773a0bf84847
Axel Garcia <Axel Garcia>
2016-06-11 22:54:22 +0200

    Tir sur le curseur Attacker

--- FafaJam_Juin2016/Assets/Scripts/AttackerManager.cs
+++ FafaJam_Juin2016/Assets/Scripts/AttackerManager.cs
@@ -18,21 +18,33 @@ public class AttackerManager : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //WeaponManager.att            
-            //Instantiate(particle, transform.position, transform.rotation);
+            Vector3 mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+            Vector3 playerPosition = gameObject.transform.position;
+            print(gameObject.transform.position);
+
+            weaponManager.directionAttack = new Vector3(mousePosition.x - playerPosition.x, mousePosition.y - playerPosition.y, playerPosition.z);
+            weaponManager.Attack(false);
         }
     }
-    
-        /*if (Input.GetKey (KeyCode.Space)) {
-			animManager.SetBool ("Firing", true);
-			if (weapon.Length != 0)
-			{
-				for (int i = 0; i < weapon.Length; i++) {
-					weapon[i].AttackWithCustomPosition (this.levelHero);
-				}
-			}
-		}
-		if(Input.GetKeyUp(KeyCode.Space))
-			animManager.SetBool("Firing", false);*/
+
+    /*if (Input.GetKey (KeyCode.Space)) {
+        animManager.SetBool ("Firing", true);
+        if (weapon.Length != 0)
+        {
+            for (int i = 0; i < weapon.Length; i++) {
+                weapon[i].AttackWithCustomPosition (this.levelHero);
+            }
+        }
+    }
+    if(Input.GetKeyUp(KeyCode.Space))
+        animManager.SetBool("Firing", false);*/
+        /*
+
+    mouse_pos = Input.mousePosition;
+     mouse_pos.z = 5.23; //The distance between the camera and object
+     object_pos = Camera.main.WorldToScreenPoint(target.position);
+     mouse_pos.x = mouse_pos.x - object_pos.x;
+     mouse_pos.y = mouse_pos.y - object_pos.y;
+     angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+     transform.rotation = Quaternion.Euler(Vector3(0, 0, angle));*/
 }

8d362cf677a6b97ece9bec2cbcce6ee73e260e4e
Axel Garcia <Axel Garcia>
2016-06-11 22:26:27 +0200

    Récupartion Pull, début du tir

--- FafaJam_Juin2016/Assets/Scripts/AttackerManager.cs
+++ FafaJam_Juin2016/Assets/Scripts/AttackerManager.cs
@@ -2,14 +2,37 @@
 using System.Collections;
 
 public class AttackerManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    private WeaponManager weaponManager;
+    void Start () {
+        weaponManager = gameObject.GetComponent<WeaponManager>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        shoot();
+
+    }
+
+    public void shoot()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //WeaponManager.att            
+            //Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+    
+        /*if (Input.GetKey (KeyCode.Space)) {
+			animManager.SetBool ("Firing", true);
+			if (weapon.Length != 0)
+			{
+				for (int i = 0; i < weapon.Length; i++) {
+					weapon[i].AttackWithCustomPosition (this.levelHero);
+				}
+			}
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+			animManager.SetBool("Firing", false);*/
 }

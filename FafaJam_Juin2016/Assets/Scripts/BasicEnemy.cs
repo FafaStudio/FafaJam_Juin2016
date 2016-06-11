d7c7084ed5aa239af2872cf6d40603d9940561b1
Hugo Bombail <hugo.bombail@wanadoo.fr>
2016-06-11 14:37:23 -0400

    Enemis de base + récupération des scripts V for Virus

---
+++ FafaJam_Juin2016/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemy : EnemyScript {
+
+	private WeaponManager weapon;
+	private float maxRate;
+
+	public MovementScript movement;
+
+	private Animator anim;
+
+	public bool isMoving = false;
+
+	public bool isSPpawnedLeft = false;
+
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+		weapon = this.GetComponent<WeaponManager> ();
+		maxRate = weapon.shootingRate;
+		movement = this.GetComponent<MovementScript> ();
+	}
+
+	void Update () {
+		if (!isSPpawnedLeft) {
+			if ((this.transform.position.x - target.transform.position.x) < 6) {
+				movement.isStopped = false;
+				movement.setDirection (1, 0);
+			} else if ((this.transform.position.x - target.transform.position.x) > 7) {
+				movement.isStopped = false;
+				movement.setDirection (-1, 0);
+			} else
+				movement.isStopped = true;
+		} else {
+			if (this.transform.position.x - target.transform.position.x < 6) {
+				movement.isStopped = false;
+				movement.setDirection (-1, 0);
+			} else if ((this.transform.position.x - target.transform.position.x) > 7) {
+				movement.isStopped = false;
+				movement.setDirection (1, 0);
+			}else
+				movement.isStopped = true;
+		}
+
+		weapon.shootingRate -= Time.deltaTime;
+		if (weapon.shootingRate <= 0) {
+			doAttack ();
+		}
+	}
+
+	private void doAttack(){
+		if(weapon!=null){
+			//anim.SetTrigger ("Firing");
+			weapon.Attack (true);
+			weapon.shootingRate = maxRate;
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "TIRPlayer") {
+			this.pv -= 1;
+			if (this.pv <= 0) {
+			//	manager.updateScore (scoreValue);
+				StartCoroutine (launchDeath ());
+			}
+		}
+	}
+
+	public IEnumerator launchDeath(){
+		anim.SetBool ("isDead", true);
+		//this.transform.localScale = new Vector3 (0.4f, 0.4f);
+		yield return new WaitForSeconds (0.2f);
+		Destroy (this.gameObject);
+	}
+
+	void OnPauseGame(){
+		this.enabled = false;
+	}
+
+	void OnResumeGame(){
+		this.enabled = true;
+	}
+}
---
+++ FafaJam_Juin2016/Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+
+public class WeaponManager : MonoBehaviour
+{
+
+	public Transform shotPrefab;
+
+	public float shootingRate = 0.25f;
+
+	private float shootCooldown;
+
+	void Start()
+	{
+		shootCooldown = 0f;
+	}
+
+	void Update()
+	{
+		if (shootCooldown > 0)
+		{
+			shootCooldown -= Time.deltaTime;
+		}
+	}
+
+	public void Attack(bool isEnemy)
+	{
+		if (CanAttack)
+		{
+			shootCooldown = shootingRate;
+			var shotTransform = Instantiate(shotPrefab) as Transform;
+			shotTransform.position = transform.position;
+
+			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+			if (shot != null)
+			{
+				shot.isEnemyShot = isEnemy;
+				if (!shot.isEnemyShot) {
+					Vector3 bullPosition = new Vector3 (transform.position.x + 10f, transform.position.y+3f, 0f);
+					shotTransform.position = bullPosition;
+				}
+			}
+		}
+	}
+		
+	public bool CanAttack
+	{
+		get
+		{
+			return shootCooldown <= 0f;
+		}
+	}
+}

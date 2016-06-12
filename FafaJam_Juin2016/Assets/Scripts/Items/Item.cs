8bfad58ff08ee612c14a13f0205be1b9b278f09c
Axel Garcia <Axel Garcia>
2016-06-12 11:08:52 +0200

    Ajout du système d'item. Bonus/Malus vie opérationnelle

---
+++ FafaJam_Juin2016/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+
+    public string itemName;
+    public bool bonus;
+    SpriteRenderer itemSprite;
+    public PlayerManager playerManager;
+
+	// Use this for initialization
+	public virtual void Start () {
+        playerManager = GameObject.FindWithTag("Player").GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public virtual void itemEffect()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            itemEffect();
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ FafaJam_Juin2016/Assets/Scripts/Items/Item_Life_Bonus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_Life_Bonus : Item {
+
+    public override void Start()
+    {
+        base.Start();
+        this.itemName = "life_bonus";
+        this.bonus = true;
+    }
+
+    public override void itemEffect()
+    {
+        this.playerManager.pvRegen(1);
+    }
+}
